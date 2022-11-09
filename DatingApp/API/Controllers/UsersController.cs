e685bd338065382defd388e5b261b43a7afca378
YanaBorodulina <123!Yecz!123>
2022-11-09 11:52:51 +0800

    Get users

---
+++ DatingApp/API/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _config;
+
+    public UsersController(DataContext config)
+    {
+        _config = config;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        return _config.Users.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        return _config.Users.Find(id);
+    }
+}
