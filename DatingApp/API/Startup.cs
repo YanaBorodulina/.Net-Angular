3433366f30e15ef932c83babc5f2dd0adf795eb6
YanaBorodulina <123!Yecz!123>
2022-11-10 13:50:49 +0800

    Angular project init

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -28,6 +28,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
@@ -52,6 +53,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
             app.UseAuthorization();
 
