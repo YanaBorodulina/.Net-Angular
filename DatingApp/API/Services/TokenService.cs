4f04086658afc4d3a8ca099f86d569363657a8b0
YanaBorodulina <123!Yecz!123>
2022-11-11 18:04:15 -0800

    token service

---
+++ DatingApp/API/Services/TokenService.cs
@@ -0,0 +1,12 @@
+﻿using API.Entities;
+using API.Interfaces;
+
+namespace API.Services;
+
+public class TokenService : ITokenService
+{
+    public string CreteToken(AppUser user)
+    {
+        throw new NotImplementedException();
+    }
+}
